0c5e2cce2fd23ec455ae1157fae3783ff14bc4c4
Asparrow13 <145340503+Asparrow13@users.noreply.github.com>
2025-12-10 19:42:12 +0100

    Modified scaffold.sh

--- server/efscaffold/scaffold.sh
+++ server/efscaffold/scaffold.sh
@@ -3,7 +3,7 @@
 # Scaffold EF Core from PostgreSQL
 # -------------------------------
 
-# تحميل متغيرات البيئة من .env
+# Downloads the environmental variables from .env
 if [ -f .env ]; then
     export $(grep -v '^#' .env | xargs)
 else
@@ -11,13 +11,14 @@ else
     exit 1
 fi
 
-# تحقق أن متغير DB_CONNECTION_STRING موجود
+# check if DB_CONNECTION_STRING variable is there
 if [ -z "$DB_CONNECTION_STRING" ]; then
     echo "DbConnectionString is not set in .env"
     exit 1
 fi
 
 # Scaffold DbContext
+dotnet tool install -g dotnet-ef
 dotnet ef dbcontext scaffold "$DB_CONNECTION_STRING" Npgsql.EntityFrameworkCore.PostgreSQL \
     --output-dir ./Entities \
     --context-dir . \
